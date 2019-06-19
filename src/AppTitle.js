2981541da5fb1cd0ffa0fdcb5afe17fffe89dc4d
John Martin <johnjamesmartin@live.com>
2019-06-19 02:10:31 +0100

    Initialize project

---
+++ src/AppTitle.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AppTitle extends Component {
+  render() {
+    return (
+      <div className="col-lg-12 app-title">
+        <h1>Markdown Previewer</h1>
+      </div>
+    );
+  }
+}
+
+export default AppTitle;
