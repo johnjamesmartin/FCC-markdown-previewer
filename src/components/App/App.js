17418cec96425e79f0382e4c55f7985d247a64e0
John Martin <johnjamesmartin@live.com>
2019-06-19 02:24:32 +0100

    Add footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,45 @@ import React from 'react';
 import { Component } from 'react';
 import './App.scss';
 import AppTitle from '../AppTitle/AppTitle';
+import Footer from '../Footer/Footer';
 import BootstrapCSSOnly from 'bootstrap-css-only';
+import marked from 'marked';
+
+/* Variables:
+ **************************************/
+const renderer = new marked.Renderer();
+const codeblock = '```npm install -g marked```';
+const editorSample = `# Header
+## Sub header
+
+[Hyperlink](https://github.com/markedjs/marked)
+
+<dl>
+  <dt>Definition list</dt>
+  <dd>A definition or description here</dd>
+  <dt>Formatting</dt>
+  <dd>*Italics*, **bold**, and <em>emphasis!</em></dd>
+</dl>
+
+<pre><code>Code here, but list below:</code></pre>
+
+1. First ordered list item
+2. Another item
+<hr/>
+> Blockquote here, codeblock below:
+
+${codeblock}
+
+\`\`\`
+// Function:
+function describeMarkdown(adjective) {
+  return "markdown is " + adjective + "!";
+}
+\`\`\`
+
+![alt text](https://i.imgur.com/QzRb2ow.png "Markdown logo")`;
+
+marked.setOptions({ breaks: true });
 
 class App extends Component {
   constructor(props) {
@@ -14,7 +52,12 @@ class App extends Component {
     this.setState({ markdown: e.target.value });
   }
   render() {
-    return <AppTitle />;
+    return (
+      <React.Fragment>
+        <AppTitle />
+        <Footer />
+      </React.Fragment>
+    );
   }
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        By John for&nbsp;
+        <a href="https://learn.freecodecamp.org/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer">
+          FreeCodeCamp
+        </a>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
